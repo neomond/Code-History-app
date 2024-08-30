808fff0b762d0656f957884ada42cc62e66c9a28
neomond <neoweise@yahoo.com>
2024-08-30 15:06:38 +0400

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Nazrin Atayeva on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Nazrin Atayeva on 30.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
